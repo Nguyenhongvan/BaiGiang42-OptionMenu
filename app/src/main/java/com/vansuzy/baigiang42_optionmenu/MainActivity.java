d0d90049444795b489fb12323d969296984107ba
Nguyenhongvan <keepsmilingtogetherhongvan@gmail.com>
2017-06-02 10:22:47 +0700

    Android Project

---
+++ app/src/main/java/com/vansuzy/baigiang42_optionmenu/MainActivity.java
@@ -0,0 +1,43 @@
+package com.vansuzy.baigiang42_optionmenu;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtMau;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControls();
+    }
+
+    private void addControls() {
+        txtMau = (TextView) findViewById(R.id.txtMau);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.mnuMauXanh) {
+            txtMau.setBackgroundColor(Color.BLUE);
+        } else if (item.getItemId() == R.id.mnuMauDo) {
+            txtMau.setBackgroundColor(Color.RED);
+        } else if (item.getItemId() == R.id.mnuMauVang) {
+            txtMau.setBackgroundColor(Color.YELLOW);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
